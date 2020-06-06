9b2304fa32f6c53b092d7ded7800594339b9156a
jakub <kuba.marcinkowski98@wp.pl>
2020-06-06 23:11:05 +0200

    add-gitignore

---
+++ src/java/zadanie5/Petla1.java
@@ -0,0 +1,10 @@
+package zadanie5;
+
+public class Petla1 {
+    public static void main (String []args){
+//        Napisz program, który wypisze liczby od 1 do 10
+        for(int i = 1 ; i < 11 ; i++){
+            System.out.println ( i );
+        }
+    }
+}
