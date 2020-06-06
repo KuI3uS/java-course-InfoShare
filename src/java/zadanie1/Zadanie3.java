4589713c39019010b01653dd74b509c88ffb1af6
jakub <kuba.marcinkowski98@wp.pl>
2020-06-06 17:03:20 +0200

    add-gitignore

---
+++ src/java/zadanie1/Zadanie3.java
@@ -0,0 +1,8 @@
+package zadanie1;
+
+public class Zadanie3 {
+    public static void main (String[]args){
+
+    }
+}
+//P/7059
---
+++ src/java/zadanie2/Zmiennoprzecinkowa.java
@@ -0,0 +1,14 @@
+package zadanie2;
+
+import java.util.Scanner;
+
+public class Zmiennoprzecinkowa {
+//    Wczytywanie danych z klawiatury
+    public static void main(String[]args){
+
+        int liczba;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("podaj liczbe");
+        liczba = ( int ) scanner.nextDouble ();
+    }
+}
