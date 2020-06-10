74aba8d23a76317344633482d3fb64effde52a95
jakub <kuba.marcinkowski98@wp.pl>
2020-06-10 14:31:31 +0200

    add-gitignore

---
+++ src/java/zadanie4/Tablica1.java
@@ -0,0 +1,11 @@
+package zadanie4;
+
+public class Tablica1 {
+    public static void main (String []args){
+//        Stwórz tablicę jednowymiarową i umieść w niej dowolne wartości
+        int[] integerArray = {1,2,3,4,5,6,7,8,9};
+        //pokazuje koncowy znak
+        int arrayLength = integerArray.length;
+        System.out.println ( arrayLength );
+    }
+}
--- src/java/zadanie6/Tablica.java
+++
@@ -1,7 +0,0 @@
-package zadanie6;
-
-public class Tablica {
-    public static void main (String []args){
-        
-    }
-}
