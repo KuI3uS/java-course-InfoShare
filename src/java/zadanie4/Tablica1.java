86cad721657c6cc41adf6b80cd0b3d9b080a16cc
jakub <kuba.marcinkowski98@wp.pl>
2020-06-15 19:27:12 +0200

    add-gitignore

--- src/java/zadanie4/Tablica1.java
+++ src/java/zadanie4/Tablica1.java
@@ -1,11 +1,20 @@
 package zadanie4;
 
+import java.awt.*;
+
 public class Tablica1 {
     public static void main (String []args){
 //        Stwórz tablicę jednowymiarową i umieść w niej dowolne wartości
-        int[] integerArray = {1,2,3,4,5,6,7,8,9};
-        //pokazuje koncowy znak
-        int arrayLength = integerArray.length;
-        System.out.println ( arrayLength );
+        int[] array = new int[10];
+        int oddSum = 0;
+
+        for (int i = 0; i <= array.length; i++) {
+            if (i % 1 == 0) {
+                oddSum += i;
+            }
+        }
+        //Wypisz na ekran tylko komórki o indeksie nieparzystym.
+
+       System.out.println ( array );
     }
 }
