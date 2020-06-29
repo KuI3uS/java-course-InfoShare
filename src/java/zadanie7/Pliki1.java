5e82a70761d64f7212d3b49c63fedbf3ed222623
jakub <kuba.marcinkowski98@wp.pl>
2020-06-29 14:39:47 +0200

    add-gitignore

--- src/java/zadanie7/Pliki1.java
+++ src/java/zadanie7/Pliki1.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 
 public class Pliki1 {
     public static void main (String []args)throws IOException {
+//        Stwórz tablicę z imionami (5) i zapisz ją do pliku.
         String [] names = {"Adam" , "Juri", "Rafał" , "Milek" , "Piotrek"};
         File pa = new File("src/java/zadanie7/plik");
         PrintWriter printWriter = new PrintWriter(pa);
---
+++ src/java/zadanie8/Pliki.java
@@ -0,0 +1,21 @@
+package zadanie8;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Scanner;
+
+public class Pliki {
+    public static void main(String []args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ( "Podaj nazwe:" );
+        String name = scanner.nextLine();
+        File plik = new File("src/java/zadanie8/elo");
+        PrintWriter writer = new PrintWriter(plik);
+        writer.println(name);
+        writer.close();
+
+    }
+
+}
